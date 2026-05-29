6fcdecfd40c0df1866c9ad0b752236a3dd9bd262
Daniel <mantisvega@gmail.com>
2026-05-29 08:46:37 -0500

    suma de arreglo,promedio,elemento_mayor

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -110,26 +110,37 @@ public class Workshop {
     return resultado;
 }
 
-    // Método que suma todos los elementos de un arreglo
     public int sumaElementos(int[] arreglo) {
-        // TODO: Implementar el método para sumar todos los elementos de un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 15.
-        return 0;
+    int suma = 0;
+    for (int num : arreglo) {
+        suma += num;
     }
+    return suma;
+}
 
-    // Método que calcula el promedio de los elementos de un arreglo
-    public double promedioElementos(int[] arreglo) {
-        // TODO: Implementar el método para calcular el promedio de los elementos de un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 3.0.
+public double promedioElementos(int[] arreglo) {
+    if (arreglo.length == 0) {
         return 0.0;
     }
-
-    // Método que encuentra el elemento mayor en un arreglo
-    public int encontrarElementoMayor(int[] arreglo) {
-        // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
-        return 0;
+    int suma = 0;
+    for (int num : arreglo) {
+        suma += num;
     }
+    return (double) suma / arreglo.length;
+}
+
+public int encontrarElementoMayor(int[] arreglo) {
+    if (arreglo.length == 0) {
+        throw new IllegalArgumentException("El arreglo no puede estar vacío.");
+    }
+    int mayor = arreglo[0];
+    for (int i = 1; i < arreglo.length; i++) {
+        if (arreglo[i] > mayor) {
+            mayor = arreglo[i];
+        }
+    }
+    return mayor;
+}
 
     // Método que encuentra el elemento menor en un arreglo
     public int encontrarElementoMenor(int[] arreglo) {
